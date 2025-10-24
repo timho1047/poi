c3a057476e255ec7e6fca2f03724a70018181f30
tim <timho1047@gmail.com>
2025-10-24 09:45:58 +0000

    traiin multiple llm

--- src/poi/llm/__init__.py
+++ src/poi/llm/__init__.py
@@ -1,6 +1,7 @@
 import unsloth  # isort: skip, to keep unsloth importted in the top
 from .config import LLMConfig
 from .inference import inference, load_fast_inference_model, load_inference_model, load_pretrained_model
+from .memory_utils import cleanup_memory, cleanup_trainer, get_memory_summary, print_memory_summary
 from .trainer import train_llm, train_llm_fast
 
 __all__ = [
@@ -11,4 +12,8 @@ __all__ = [
     "load_fast_inference_model",
     "inference",
     "train_llm_fast",
+    "cleanup_memory",
+    "cleanup_trainer",
+    "get_memory_summary",
+    "print_memory_summary",
 ]

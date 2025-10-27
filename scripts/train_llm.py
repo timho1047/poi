2b3bdbe98e8b1c129697f29bafc488073276f739
Tim HO <94374732+timho1047@users.noreply.github.com>
2025-10-28 00:51:16 +0800

    eval paper base on 64bs

--- scripts/train_llm.py
+++ scripts/train_llm.py
@@ -7,7 +7,7 @@ from poi.llm.trainer import train_llm_fast
 
 if __name__ == "__main__":
     config = LLMConfig(
-        run_name="llama3-nyc-test-full-8", num_epochs=8, batch_size=4, gradient_accumulation_steps=4, do_eval=False, resume_from_checkpoint=False
+        run_name="llama3-nyc-test", num_epochs=8, batch_size=8, gradient_accumulation_steps=8, do_eval=True, resume_from_checkpoint=False
     )
 
     DATASET_DIR = settings.DATASETS_DIR / "NYC" / "LLM Dataset" / "paper"
@@ -24,4 +24,4 @@ if __name__ == "__main__":
     print(f"Eval dataset size: {len(eval_dataset)} examples")
 
     # if push_to_hub is True, the model will be pushed to the Hugging Face hub repo, otherwise it will be saved to the local directory
-    train_llm_fast(config, train_dataset, None, push_to_hub=True)
+    train_llm_fast(config, train_dataset, eval_dataset, push_to_hub=True)

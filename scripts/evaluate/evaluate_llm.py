dd2169129b530ff09575fad0c505d9a1dcb32f3b
Tim HO <94374732+timho1047@users.noreply.github.com>
2025-10-31 16:21:04 +0800

    verify dataset

--- scripts/evaluate/evaluate_llm.py
+++ scripts/evaluate/evaluate_llm.py
@@ -3,6 +3,9 @@ from poi.llm import LLMConfig
 from poi.llm.evaluate import evaluate_all_and_save
 
 if __name__ == "__main__":
+    NYC_PAPER_BASE_DS = settings.DATASETS_DIR / "NYC" / "LLM Dataset" / "paper-nyc-base"
+    NYC_PAPER_NO_SID_DS = settings.DATASETS_DIR / "NYC" / "LLM Dataset" / "paper-nyc-no-sid"
+    
     NYC_BASE_DS = settings.DATASETS_DIR / "NYC" / "LLM Dataset" / "rqvae-nyc-div0.25-commit0.5-lr5e-5"
     NYC_NO_TIME_DS = settings.DATASETS_DIR / "NYC" / "LLM Dataset" / "ablation without Time"
     NYC_NO_DIV_DS = settings.DATASETS_DIR / "NYC" / "LLM Dataset" / "rqvae-nyc-div0.0-commit0.5-lr5e-5"
@@ -19,6 +22,7 @@ if __name__ == "__main__":
     TKY_DIV_0_5_DS = settings.DATASETS_DIR / "TKY" / "LLM Dataset" / "rqvae-tky-div0.5-commit0.25-lr5e-5"
     TKY_DIV_0_75_DS = settings.DATASETS_DIR / "TKY" / "LLM Dataset" / "rqvae-tky-div0.75-commit0.25-lr5e-5"
 
+    # NYC
     CONFIG_DS_DIR_PAIRS = [
         (LLMConfig(run_name="llama3-nyc-base"), NYC_BASE_DS),
         (LLMConfig(run_name="llama3-nyc-no-time"), NYC_NO_TIME_DS),
@@ -29,6 +33,7 @@ if __name__ == "__main__":
         (LLMConfig(run_name="llama3-nyc-div-0.75"), NYC_DIV_0_75_DS),
     ]
 
+    # TKY
     CONFIG_DS_DIR_PAIRS += [
         (LLMConfig(run_name="llama3-tky-base"), TKY_BASE_DS),
         (LLMConfig(run_name="llama3-tky-no-time"), TKY_NO_TIME_DS),
@@ -37,5 +42,12 @@ if __name__ == "__main__":
         (LLMConfig(run_name="llama3-tky-no-quant"), TKY_NO_QUANT_DS),
         (LLMConfig(run_name="llama3-tky-div-0.5"), TKY_DIV_0_5_DS),
         (LLMConfig(run_name="llama3-tky-div-0.75"), TKY_DIV_0_75_DS),
+    ]
+    
+    # Paper
+    CONFIG_DS_DIR_PAIRS += [
+        (LLMConfig(run_name="llama3-nyc-base-8-epochs"), NYC_BASE_DS),
+        (LLMConfig(run_name="llama3-nyc-test-no-sid"), NYC_PAPER_NO_SID_DS),
+        (LLMConfig(run_name="llama3-nyc-test"), NYC_PAPER_BASE_DS),
     ]
     evaluate_all_and_save(CONFIG_DS_DIR_PAIRS, settings.OUTPUT_DIR / "llm_metrics.csv")
--- src/poi/llm/evaluate.py
+++ src/poi/llm/evaluate.py
@@ -45,12 +45,12 @@ def evaluate_model(config: LLMConfig, ds_dir: Path, from_hub: bool = True):
             test_all_ds_path = file
         elif file.name.startswith("test"):
             test_ds_path = file
-    if not all([train_ds_path, val_ds_path, test_all_ds_path, test_ds_path]):
-        raise ValueError(f"No train, val, test or test_all dataset found in {ds_dir}")
+    if not all([train_ds_path, test_ds_path]):
+        raise ValueError(f"No train or test dataset found in {ds_dir}")
 
     train_ds = load_prompt_completion_llm_dataset(train_ds_path)
-    val_ds = load_prompt_completion_llm_dataset(val_ds_path)
-    test_all_ds = load_prompt_completion_llm_dataset(test_all_ds_path)
+    val_ds = load_prompt_completion_llm_dataset(val_ds_path) if val_ds_path else None
+    test_all_ds = load_prompt_completion_llm_dataset(test_all_ds_path) if test_all_ds_path else None
     test_ds = load_prompt_completion_llm_dataset(test_ds_path)
 
     is_sid = "codebook" in train_ds_path.name.lower()
@@ -58,14 +58,14 @@ def evaluate_model(config: LLMConfig, ds_dir: Path, from_hub: bool = True):
     model = load_fast_inference_model(config, from_hub=from_hub)
 
     train_acc = top_one_accuracy(config, model, train_ds, is_sid)
-    val_acc = top_one_accuracy(config, model, val_ds, is_sid)
-    test_all_acc = top_one_accuracy(config, model, test_all_ds, is_sid)
+    val_acc = top_one_accuracy(config, model, val_ds, is_sid) if val_ds else None
+    test_all_acc = top_one_accuracy(config, model, test_all_ds, is_sid) if test_all_ds else None
     test_acc = top_one_accuracy(config, model, test_ds, is_sid)
 
     return {
         "train_acc": train_acc,
-        "val_acc": val_acc,
-        "test_all_acc": test_all_acc,
+        "val_acc": val_acc if val_acc else None,
+        "test_all_acc": test_all_acc if test_all_acc else None,
         "test_acc": test_acc,
     }
 
@@ -83,10 +83,16 @@ def evaluate_all_and_save(config_ds_dir_pairs: list[tuple[LLMConfig, Path]], sav
             pbar.update(1)
             continue
         pbar.set_description(f"Evaluating {config.run_name}")
+        
         metrics = evaluate_model(config, ds_dir, from_hub)
+        train_acc = f"{metrics['train_acc']:.4f}"
+        test_acc = f"{metrics['test_acc']:.4f}"
+        val_acc = f"{metrics['val_acc']:.4f}" if metrics['val_acc'] else "-"
+        test_all_acc = f"{metrics['test_all_acc']:.4f}" if metrics['test_all_acc'] else "-"
+        
         with save_path.open("a") as f:
             f.write(
-                f"{config.run_name},{metrics['train_acc']:.4f},{metrics['val_acc']:.4f},{metrics['test_all_acc']:.4f},{metrics['test_acc']:.4f}\n"
+                f"{config.run_name},{train_acc},{val_acc},{test_all_acc},{test_acc}\n"
             )
-        pbar.write(f"Evaluated {config.run_name} with metrics: {metrics}")
+        pbar.write(f"Evaluated {config.run_name} with metrics: train_acc={train_acc}, val_acc={val_acc}, test_all_acc={test_all_acc}, test_acc={test_acc}")
         pbar.update(1)

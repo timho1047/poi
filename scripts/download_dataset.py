61f293c4de8ef193faf9601111c41964300e5b59
Tim HO <94374732+timho1047@users.noreply.github.com>
2025-11-08 12:37:24 +0800

    download exploration dataset

--- scripts/download_dataset.py
+++ scripts/download_dataset.py
@@ -3,3 +3,5 @@ from poi.dataset.download import download_dataset
 if __name__ == "__main__":
     download_dataset("NYC")
     download_dataset("TKY")
+    download_dataset("NYC_Exploration")
+    download_dataset("TKY_Exploration")
